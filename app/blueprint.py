0f320d8280ce7fdca2c8b10eed54235e48a34d9a
Nomuntu <86933978+Nomuntu@users.noreply.github.com>
2025-07-03 20:16:37 +0200

    Update blueprint.py

--- app/blueprint.py
+++ app/blueprint.py
@@ -19,8 +19,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
