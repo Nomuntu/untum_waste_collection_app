4575bb10049dc3ac2168777b82bd9a1143b5a007
Nomuntu <86933978+Nomuntu@users.noreply.github.com>
2025-07-04 01:17:19 +0200

    Add files via upload

--- app/blueprint.py
+++ app/blueprint.py
@@ -23,6 +23,11 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u3e45j6sr7bg30:p22c3db9b066639d40bc0191a61d668880c84f52998f4cf5345c97a94e07ed874@cd7f19r8oktbkp.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/db2ie3q7cgm6pp'
+    if not app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+            'pool_size': 10,
+            'max_overflow': 20
+        }
     db = SQLAlchemy(app)
     migrate = Migrate(app, db)
 
@@ -63,6 +68,7 @@ def create_app(config_object: AppConfig) -> Flask:
     sess.init_app(app)
     socketio.init_app(app)
 
+    app.session_interface.db.create_all()
     with app.app_context():
         db.create_all()
 
