775d9c159175198c7d7bcb849759b5535af1cf80
Nomuntu <86933978+Nomuntu@users.noreply.github.com>
2025-07-11 12:16:36 +0200

    Update app_config.py

--- app/app_config.py
+++ app/app_config.py
@@ -6,6 +6,14 @@ class AppConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'sqlalchemy'
     SESSION_USE_SIGNER = True
+    TIMEZONE = 'Africa/Johannesburg'
+    SMS_EXPIRY_MINS = 60
+    WASTE_MANAGEMENT_PROVIDER_NAME = 'Untum'
+    ADMIN_INITIAL_PASSWORD = 'admin123'
+    BAG_SIZES = [5, 10, 20, 25 , 50 , 1000 ]
+    MATERIAL_TYPES = ['LD Plastic', 'Glass', 'Cans', 'Cardboard', 'Paper', 'PET', 'HD', 'Common', 'Carton']
+    PHOTO_WIDTH = 800
+
 
 class DevelopmentConfig(AppConfig):
     DEBUG = True
