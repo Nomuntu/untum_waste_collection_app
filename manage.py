c69c805c570d5441ca470383d4fc283948a40580
Nomuntu <86933978+Nomuntu@users.noreply.github.com>
2025-07-28 14:37:03 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 from flask.cli import FlaskGroup
-from app.blueprint import create_app, db
+from app import create_app, db
+from flask_migrate import MigrateCommand
 
 app = create_app()
 cli = FlaskGroup(app)
+
+# Optional: add Flask-Migrate db command
+cli.add_command("db", MigrateCommand)
 
 if __name__ == "__main__":
     cli()
